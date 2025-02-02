3e8c9173e7c49efdd4be8f93f8bd21a2dca80e86
Danyil <tribeofdanel@gmail.com>
2025-02-02 10:55:48 +0200

    updte

--- src/index.ts
+++ src/index.ts
@@ -104,40 +104,10 @@ export function getTokenForProvider(
       return (
         character.settings?.secrets?.OPENAI_API_KEY || settings.OPENAI_API_KEY
       );
-    case ModelProviderName.LLAMACLOUD:
-      return (
-        character.settings?.secrets?.LLAMACLOUD_API_KEY ||
-        settings.LLAMACLOUD_API_KEY ||
-        character.settings?.secrets?.TOGETHER_API_KEY ||
-        settings.TOGETHER_API_KEY ||
-        character.settings?.secrets?.XAI_API_KEY ||
-        settings.XAI_API_KEY ||
-        character.settings?.secrets?.OPENAI_API_KEY ||
-        settings.OPENAI_API_KEY
-      );
-    case ModelProviderName.ANTHROPIC:
-      return (
-        character.settings?.secrets?.ANTHROPIC_API_KEY ||
-        character.settings?.secrets?.CLAUDE_API_KEY ||
-        settings.ANTHROPIC_API_KEY ||
-        settings.CLAUDE_API_KEY
-      );
-    case ModelProviderName.REDPILL:
-      return (
-        character.settings?.secrets?.REDPILL_API_KEY || settings.REDPILL_API_KEY
-      );
     case ModelProviderName.OPENROUTER:
       return (
         character.settings?.secrets?.OPENROUTER || settings.OPENROUTER_API_KEY
       );
-    case ModelProviderName.GROK:
-      return character.settings?.secrets?.GROK_API_KEY || settings.GROK_API_KEY;
-    case ModelProviderName.HEURIST:
-      return (
-        character.settings?.secrets?.HEURIST_API_KEY || settings.HEURIST_API_KEY
-      );
-    case ModelProviderName.GROQ:
-      return character.settings?.secrets?.GROQ_API_KEY || settings.GROQ_API_KEY;
   }
 }
 
@@ -308,17 +278,21 @@ async function handleUserInput(input, agentId) {
   try {
     const serverPort = parseInt(settings.SERVER_PORT || "3000");
 
-    const response = await fetch(`${API_URL}/${agentId}/message`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        text: input,
-        userId: "user",
-        userName: "User",
-      }),
-    });
+    const response = await fetch(
+      `${API_URL}:${serverPort}/${agentId}/message`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          text: input,
+          userId: "user",
+          userName: "User",
+        }),
+      },
+    );
 
     const data = await response.json();
+    console.log(data);
     data.forEach((message) => console.log(`${"Agent"}: ${message.text}`));
   } catch (error) {
     console.error("Error fetching response:", error);
